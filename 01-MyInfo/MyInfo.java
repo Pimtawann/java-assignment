50795f26ba5fbb09d9bd1512eca25c87ccb25d15
Pimtawan Wonnawijit <pimtawan@MacBook-Air-khxng-Pimtawan.local>
2025-11-06 09:49:45 +0700

    feat: answer exercise 1

---
+++ 01-MyInfo/MyInfo.java
@@ -0,0 +1,16 @@
+public class MyInfo {
+    public MyInfo() {
+        
+    }
+
+    public static void main(String[] args){
+        String name = "Pimtawan";
+        int age = 25;
+        String hobby = "Listening to music";
+
+        System.out.println("My name is " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+    }
+    
+}
